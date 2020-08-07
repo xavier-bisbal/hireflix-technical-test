73c55b132eb23eff9c8406ec9f8a12e952f41f8f
xavierbisbal <xbisbal@hotmail.com>
2020-08-07 19:32:24 +0200

    Implement getInterview

--- apis/middleware/src/services/api.js
+++ apis/middleware/src/services/api.js
@@ -22,7 +22,29 @@ const api = ({ endpoint = '/', data = undefined, options = {} }) => {
       ...(config.api.key && { 'x-api-key': config.api.key }),
       ...headers,
     },
-  }).then((res) => res.data);
+  }).then((res) => res.data).catch((error) => {
+    // Error 😨
+    if (error.response) {
+        /*
+         * The request was made and the server responded with a
+         * status code that falls out of the range of 2xx
+         */
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    } else if (error.request) {
+        /*
+         * The request was made but no response was received, `error.request`
+         * is an instance of XMLHttpRequest in the browser and an instance
+         * of http.ClientRequest in Node.js
+         */
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
 };
 
 const getPositionInfo = (positionId) => {
@@ -46,14 +68,64 @@ const getPositionInfo = (positionId) => {
         }
       `,
     },
-  }).then((response) => {
-    return response.data.position;
+  }).then((res) => {
+    return res.data.position;
   });
 };
 
 const getInterview = (interviewId) => {
   console.log(`search on api details of interview ${interviewId}`);
-  return Promise.resolve({});
+  return api({
+    endpoint: '/me',
+    data: {
+      variables: JSON.stringify({ interviewId }),
+      query: `
+        query getInterview($interviewId: String!) {
+          interview(id: $interviewId) {
+            id
+            status
+            createdAt
+            updatedAt
+            thumbnail
+            candidate {
+              name
+              email
+              phone
+            }
+            inviteSource
+            externalLink {
+              id
+              expires
+            }
+            timeToThink
+            timeToAnswer
+            notes
+            questions {
+              id
+              title
+              description
+              notes
+              answered
+              score
+              media {
+                thumbnail
+                url
+              }
+              answer {
+                thumbnail
+                url
+                meta {
+                  duration
+                }
+              }
+            }
+          }
+        }
+      `,
+    },
+  }).then((res) => {
+    return res.data.interview;
+  });
 };
 
 const inviteCandidate = (positionId, name, email, phone) => {
