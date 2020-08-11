0436ac642073a69481f9f1d6b4e88ef7b9ac5c93
xavierbisbal <xbisbal@hotmail.com>
2020-08-11 16:23:09 +0200

    Define api routes

--- apis/middleware/src/routes/v1/v1.js
+++ apis/middleware/src/routes/v1/v1.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getPosition, getInterview, inviteCandidate } from 'services/api';
 
 const router = express.Router();
 
@@ -6,6 +7,21 @@ router.get('/version', (req, res) => {
   res.json({
     version: process.env.VERSION || 'no version available',
   });
+});
+
+router.get('/position/:positionId', async (req, res) => {
+  const position = await getPosition(req.params.positionId);
+  res.json({ position });
+});
+
+router.get('/interview/:interviewId', async (req, res) => {
+  const interview = await getInterview(req.params.interviewId);
+  res.json({ interview });
+});
+
+router.get('/invite/:positionId/:name/:email/:phone', async (req, res) => {
+  const interview = await inviteCandidate(req.params.positionId, req.params.name, req.params.email, req.params.phone);
+  res.json({ interview });
 });
 
 export default router;
